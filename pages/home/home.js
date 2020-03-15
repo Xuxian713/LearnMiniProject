28799ce0922680f1ef9c013dc2891ed2978ede74
xuxian <Xuxian713@163.com>
2020-03-15 15:25:46 +0800

    列表v-model

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name:'你好! Bob',
+    age:'我今年一岁半了!',
+    pro:[
+      { id: '1',  user:'user'  },
+      { id: '1', user: 'user' },
+      { id: '1', user: 'user' },
+    ],
+    counter:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onClickAdd(){
+    this.setData({
+      counter: this.data.counter +=1
+  })
+  } ,
+  onClickReduce(){
+    this.setData({
+      counter: this.data.counter -=1
+    })
   }
 })
